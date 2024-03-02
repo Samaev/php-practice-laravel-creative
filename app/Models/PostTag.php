edf5e71af238a4e3ee986a585f399ee1d0aaaaf6
Andrey Samaev <andreysamaev@gmail.com>
2024-03-02 17:24:24 +0200

    titles added, tags implemented

--- app/Models/PostTag.php
+++ app/Models/PostTag.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class PostTag extends Model
 {
     use HasFactory;
-
-    public function posts()
-    {
-        return $this->hasMany(Post::class, 'post_id', 'id');
-    }
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,16 +8,18 @@
 
 
     <div class="row justify-content-center">
-        @foreach($posts as $key=>$post)
+        @foreach($posts as $post)
             <div class="card m-2 col-11 col-md-5 p-0">
                 <div class="card-header text-center">
-                    <h3><a href="{{ route('post.show', $post->id) }}">{{ ++$key }}. {{ $post->title }}</a></h3>
+                    <h3 class="card-title"><a href="{{ route('post.show', $post->id) }}">{{ $post->title }}</a></h3>
                 </div>
                 <div class="card-body">
                     <span>{{ $post->post_content }} - </span>
                 </div>
                 <div class="card-footer">
-                    <span>{{ $post->likes }}</span>
+                    <button type="button" class="btn btn-primary">
+                        Likes: <span class="badge bg-danger">{{ $post->likes }}</span>
+                    </button>
                     <span>{{ $post->image }}</span>
                 </div>
             </div>
